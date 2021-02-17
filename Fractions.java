151aa969b92a16e80de8313b3efe1dd8bbfea4af
jake89724 <72215471+jake89724@users.noreply.github.com>
2021-02-16 22:34:20 -0600

    Add files via upload

--- Fractions.java
+++ Fractions.java
@@ -8,23 +8,33 @@ package fractions;
 import javax.swing.JOptionPane;
 
 /**
- *
- * @author j.head
+ * Teacher: Mr.Wachs 
+ * February 15th 2021 
+ * Reduces Fractions
+ * @author JAKE
  */
 public class Fractions {
 
+    //a very proffessional constant 
     final public static int A_BIG_NUMBER = 100;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String userInput = JOptionPane.showInputDialog("Enter Fraction");
+        String userInput = JOptionPane.showInputDialog(null,
+                "Enter Fraction", "ENTER", JOptionPane.QUESTION_MESSAGE);
         int numerator = getNumerator(userInput);
         int denominator = getDenominator(userInput);
         reduce(denominator, numerator);
     }
 
+    /**
+     * Gets the numerator and converts to int
+     *
+     * @param takes in the user input as a string
+     * @return the individual numerator as an int
+     */
     private static int getNumerator(String userInput) {
         String main = "";
         boolean stop = false;
@@ -41,6 +51,12 @@ public class Fractions {
         return (numerator);
     }
 
+    /**
+     * Gets denominator and converts it to an int
+     *
+     * @param userInput Takes in the user input as a string
+     * @return The denominator as an int
+     */
     private static int getDenominator(String userInput) {
         String main = "";
         boolean start = false;
@@ -56,35 +72,29 @@ public class Fractions {
         return (Denominator);
     }
 
+    /**
+     * reduces both numerator and denominator
+     *
+     * @param denominator the denominator as an int
+     * @param numerator the numerator as an int
+     */
     private static void reduce(int denominator, int numerator) {
-        int den = denominator;
-        int num = numerator;
+        boolean stop = false;
         for (int i = 1; i < A_BIG_NUMBER; i++) {
-            System.out.println(den % i);
-            System.out.println(num % i);
-            if (den % i == 0 && num % i == 0) {
-
-                System.out.println("made it");
-                if (num / i != 0 || den / i != 0) {
-                    den /= i;
-                    num /= i;
-                    System.out.println("den " + num);
+            stop = false;
+            while (stop == false) {
+                if (numerator % i == 0 && denominator % i == 0) {
+                    numerator /= i;
+                    denominator /= i;
+                    if (i == 1) {
+                        stop = true;
+                    }
                 } else {
-                    i = A_BIG_NUMBER;
-                    JOptionPane.showMessageDialog(null,
-                            num + "/" + den);
+                    stop = true;
                 }
-
-                System.out.println(den);
-                System.out.println(num);
-            } else {
-                i = A_BIG_NUMBER;
-                JOptionPane.showMessageDialog(null,
-                        num + "/" + den);
             }
-
         }
-
+        JOptionPane.showMessageDialog(null, numerator + "/" + denominator,
+                "Answer!", JOptionPane.INFORMATION_MESSAGE);
     }
-
 }
